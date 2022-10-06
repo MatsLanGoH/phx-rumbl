e16167b77ad8012aeb1aaf7a7f9fe0b1a04111a0
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2022-10-06 22:45:28 +0200

    Add plug test

--- config/test.exs
+++ config/test.exs
@@ -28,3 +28,6 @@ config :logger, level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Limit number of hashing rounds to speed up test execution
+config :pbkdf2_elixir, :rounds, 1
